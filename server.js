cc13fbaec5b1ac5383187a81c6b3539c7d48ab4b
raju-ranjan <raju.ranjan5@gmail.com>
2022-05-12 10:32:24 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,14 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('\n Welcome Wing 2 Technical Architect Devops Program \n');
+  res.send('\n \n \n Welcome to Wing 2 Technical Architect Devops Program \n \n');
 });
 
+app.get('/health-check',(req,res)=> {
+  res.send ("Health check passed");
+});
+app.get('/bad-health',(req,res)=> {
+  res.status(500).send('Health check did not pass');
+});
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
